6fd7e45c478b4944e565dbe3146d6ec838379fee
V <vijays211251@gmail.com>
2025-04-23 12:30:50 +0530

    Add files via upload

---
+++ S3.tf
@@ -0,0 +1,3 @@
+resource "aws_s3_bucket" "e5mple" {
+  bucket = "yuiisjkfhrhwkess-bucket66565"
+}
---
+++ ec2.tf
@@ -0,0 +1,4 @@
+#resource "aws_instance" "web" {
+ # ami           = "ami-00a929b66ed6e0de6"
+  #instance_type = "t2.micro"
+#}
